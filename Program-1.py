0a8caf7859f9873dd6fa011b39bf82a350573bd9
anusha8019 <anushajalla2@gmail.com>
2025-08-11 14:07:03 +0530

    Add files via upload

---
+++ Program-1.py
@@ -0,0 +1,33 @@
+'''
+Problem-1: Create a small calculator which performs operations such as Addition, Subtraction, Multiplication and Division using class.
+  Calculator inputs :> ‘a’, ‘b’ and ‘type of operation’
+  Datatype :> ‘a’ = double, ‘b’ = double, ‘type of operation’ = string
+'''
+class Calculator:
+    def __init__(self, a, b, operation):
+        self.a = a
+        self.b = b
+        self.operation = operation
+
+    def calculate(self):
+        if self.operation == "add":
+            return self.a + self.b
+        elif self.operation == "subtract":
+            return self.a - self.b
+        elif self.operation == "multiply":
+            return self.a * self.b
+        elif self.operation == "divide":
+            if self.b != 0:
+                return self.a / self.b
+            else:
+                return "Cannot divide by zero"
+        else:
+            return "Invalid operation"
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+operation = input("Enter operation (add, subtract, multiply, divide): ").lower()
+
+calc = Calculator(a, b, operation)  
+print("Result:", calc.calculate())
